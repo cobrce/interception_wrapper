894fb3b336e75404408362327c4b8249796cb336
cob_258 <cob_rce@hotmail.com>
2019-01-28 02:06:13 +0100

    Fixed error in mouse_stroke

--- stroke.py
+++ stroke.py
@@ -7,7 +7,8 @@ class stroke():
 
 class mouse_stroke(stroke):
 
-    fmt = 'HHhiiI'    
+    fmt = 'HHhiiI'
+    fmt_raw = 'HHHHIiiI'
     state = 0
     flags = 0
     rolling = 0
@@ -15,12 +16,25 @@ class mouse_stroke(stroke):
     y = 0    
     information = 0
 
-    def __init__(self):
+    
+
+    def __init__(self,state,flags,rolling,x,y,information):
         super().__init__()
+        self.state =state
+        self.flags = flags
+        self.rolling = rolling
+        self.x = x
+        self.y = y
+        self.information = information
     
     @staticmethod
     def parse(data):
-        return key_stroke(*struct.unpack(mouse_stroke.fmt,data))        
+        return mouse_stroke(*struct.unpack(mouse_stroke.fmt,data))        
+
+    @staticmethod
+    def parse_raw(data):
+        unpacked= struct.unpack(mouse_stroke.fmt_raw,data)
+        return  mouse_stroke(unpacked[1],unpacked[2],unpacked[3],unpacked[5],unpacked[6],unpacked[7])
 
     @property
     def data(self):
@@ -32,6 +46,7 @@ class mouse_stroke(stroke):
 class key_stroke(stroke):
 
     fmt = 'HHI'
+    fmt_raw = 'HHHHI'
     code = 0
     state = 0
     information = 0
@@ -47,6 +62,11 @@ class key_stroke(stroke):
     def parse(data):
         return key_stroke(*struct.unpack(key_stroke.fmt,data))
     
+    @staticmethod
+    def parse_raw(data):
+        unpacked= struct.unpack(key_stroke.fmt_raw,data)
+        return  key_stroke(unpacked[1],unpacked[2],unpacked[4])
+
     @property
     def data(self):
         data = struct.pack(self.fmt,self.code,self.state,self.information)
